c3c5c49c995428cc89f894af1f6b1109296abd2e
Antoine Romand <antoine.romand@epitech.eu>
2025-04-02 00:34:19 +0200

    feat: Add Jwt lib and spring core, start implementing login usecase.

---
+++ lib/src/main/java/com/dxs/auth/core/entity/AbstractUser.java
@@ -0,0 +1,10 @@
+package com.dxs.auth.core.entity;
+
+import java.util.UUID;
+
+public abstract class AbstractUser {
+    public abstract UUID getId();
+    public abstract String getEmail();
+    public abstract String getPassword();
+    public abstract RoleEnum getRole();
+}
