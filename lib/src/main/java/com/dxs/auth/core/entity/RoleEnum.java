476e5def39e92b43c2193a8fb8092590789a6c06
Antoine Romand <antoine.romand@epitech.eu>
2025-05-10 16:13:37 +0200

    feat: add more role in RoleEnum

--- lib/src/main/java/com/dxs/auth/core/entity/RoleEnum.java
+++ lib/src/main/java/com/dxs/auth/core/entity/RoleEnum.java
@@ -2,5 +2,14 @@ package com.dxs.auth.core.entity;
 
 public enum RoleEnum {
     ADMIN,
-    REGULAR
+    REGULAR,
+    CONSUMER,
+    SELLER,
+    PRO,
+    PREMIUM,
+    USER_REGULAR,
+    USER_PREMIUM,
+    USER_PRO,
+    SELLER_PREMIUM,
+    SELLER_PRO,
 }
